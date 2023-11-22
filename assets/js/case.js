f3225d0c5df143a1490cbb52f819a01bcd413657
Habibur_Rahman <hr16792@gmail.com>
2023-11-22 17:58:40 +0600

    case section added

--- assets/js/case.js
+++ assets/js/case.js
@@ -1,6 +1,40 @@
 // body section start
+document.getElementById('btn-case').addEventListener('click', function(){
+    const caseField = document.getElementById('input-case');
+    const caseFieldValue = caseField.value ;
+    const caseNumber = parseInt (caseFieldValue);
+
+    const previousCaseValue = caseNumber + 1 ;
+    caseField.value = previousCaseValue;
+
+    
+    const dollerField = document.getElementById('doller');
+    const newDollerField = dollerField.innerText;
+    const previousDoller = parseInt (newDollerField);
+
+    const previousDollerValue = previousCaseValue  * 49;
+    dollerField.innerText = previousDollerValue;
+
+  
+} )
+
+document.getElementById('btn-case-1').addEventListener('click', function(){
+    const caseField = document.getElementById('input-case');
+    const caseFieldValue = caseField.value ;
+    const caseNumber = parseInt (caseFieldValue);
+
+    const previousCaseValue = caseNumber - 1 ;
+    caseField.value = previousCaseValue;
+
+    const dollerField = document.getElementById('doller');
+    const newDollerField = dollerField.innerText;
+    const previousDoller = parseInt (newDollerField);
 
 
+    const previousDollerValue = previousCaseValue * 49;
+    dollerField.innerText = previousDollerValue;
+
+})
 
 
 // body section end
